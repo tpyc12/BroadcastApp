46017488d400d98cfdc91674bf6440bece23fba8
tpyc12 <tpyc12@yandex.ru>
2022-09-11 18:53:34 +0300

    feat: create broadcast receiver

--- app/src/main/java/com/myhome/android/broadcastapp/MainActivity.kt
+++ app/src/main/java/com/myhome/android/broadcastapp/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.myhome.android.broadcastapp
 
+import android.content.Intent
+import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    private val receiver = MyReceiver()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val intentFilter = IntentFilter(Intent.ACTION_AIRPLANE_MODE_CHANGED)
+        val intentFilterBattery = IntentFilter(Intent.ACTION_BATTERY_LOW)
+        registerReceiver(receiver, intentFilterBattery)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unregisterReceiver(receiver)
     }
 }
---
+++ app/src/main/java/com/myhome/android/broadcastapp/MyReceiver.kt
@@ -0,0 +1,20 @@
+package com.myhome.android.broadcastapp
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+
+class MyReceiver: BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent?) {
+        when(intent?.action){
+            Intent.ACTION_AIRPLANE_MODE_CHANGED -> {
+                Toast.makeText(context, "Airplane mode changed", Toast.LENGTH_SHORT).show()
+            }
+            Intent.ACTION_BATTERY_LOW -> {
+                Toast.makeText(context, "Battery low", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
