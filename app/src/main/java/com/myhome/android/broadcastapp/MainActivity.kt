fa9d500f6dbc8473a750214c4b244523f5c31122
tpyc12 <tpyc12@yandex.ru>
2022-09-11 19:20:38 +0300

    feat: create multiple broadcast receiver feat: create my broadcast receiver

--- app/src/main/java/com/myhome/android/broadcastapp/MainActivity.kt
+++ app/src/main/java/com/myhome/android/broadcastapp/MainActivity.kt
@@ -2,19 +2,31 @@ package com.myhome.android.broadcastapp
 
 import android.content.Intent
 import android.content.IntentFilter
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     private val receiver = MyReceiver()
 
+    private var count = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val intentFilter = IntentFilter(Intent.ACTION_AIRPLANE_MODE_CHANGED)
-        val intentFilterBattery = IntentFilter(Intent.ACTION_BATTERY_LOW)
-        registerReceiver(receiver, intentFilterBattery)
+        findViewById<TextView>(R.id.tvHello).setOnClickListener {
+            Intent(MyReceiver.ACTION_CLICKED).apply {
+                putExtra("Count", count++)
+                sendBroadcast(this)
+            }
+        }
+        val intentFilter = IntentFilter().apply {
+            addAction(Intent.ACTION_AIRPLANE_MODE_CHANGED)
+            addAction(Intent.ACTION_BATTERY_LOW)
+            addAction(MyReceiver.ACTION_CLICKED)
+        }
+        registerReceiver(receiver, intentFilter)
     }
 
     override fun onDestroy() {
--- app/src/main/java/com/myhome/android/broadcastapp/MyReceiver.kt
+++ app/src/main/java/com/myhome/android/broadcastapp/MyReceiver.kt
@@ -9,12 +9,22 @@ class MyReceiver: BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent?) {
         when(intent?.action){
+            ACTION_CLICKED -> {
+                val count = intent.getIntExtra("Count", 0)
+                Toast.makeText(context, "Click $count", Toast.LENGTH_SHORT).show()
+            }
             Intent.ACTION_AIRPLANE_MODE_CHANGED -> {
                 Toast.makeText(context, "Airplane mode changed", Toast.LENGTH_SHORT).show()
             }
             Intent.ACTION_BATTERY_LOW -> {
-                Toast.makeText(context, "Battery low", Toast.LENGTH_SHORT).show()
+                val turnedOn = intent.getBooleanExtra("state", false)
+                Toast.makeText(context, "Battery low $turnedOn", Toast.LENGTH_SHORT).show()
             }
         }
+    }
+
+    companion object{
+
+        const val ACTION_CLICKED = "clicked"
     }
 }
